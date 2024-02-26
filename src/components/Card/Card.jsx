f6b5b8a338ff5c833002b14f6460459cb877e964
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-26 20:41:57 +0300

    add react circular progressive bar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import  './Card.css'
 import { LayoutGroup} from 'framer-motion'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Card(props) {
     const [expanded, setExpanded] = useState(false)
@@ -24,9 +27,16 @@ function CompactCard({param}){
     // console.log(param.png);
     return ( 
     
-    <div className="compactCard">
+    <div className="compactCard"
+    style={{background: param.color.backGround,
+        boxShadow: param.color.boxShadow
+    }}
+    >
         <div className="radialBar">
-            Chart
+            <CircularProgressbar
+            value={param.barValue}
+            text={param.barValue}
+            />
         </div>
         <div className="detail">
             
